0deef2af2fb4779f5ea5f5a939c16dcdb2b883b5
root <root@alfinkhalaj>
2025-05-18 23:05:48 -1200

    pemweb

---
+++ uts/coding/src/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\GuruPublikController;
+use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
+use App\Models\Guru;
+
+/* NOTE: Do Not Remove
+/ Livewire asset handling if using sub folder in domain
+*/
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::post(config('app.asset_prefix') . '/livewire/update', $handle);
+});
+
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get(config('app.asset_prefix') . '/livewire/livewire.js', $handle);
+});
+/*
+/ END
+*/
+Route::get('/', [GuruPublikController::class, 'index']);
+
+
+Route::get('/lihat-guru', [GuruPublikController::class, 'index']);
+
+Route::get('/', function () {
+    return view('home'); // Halaman awal
+});
+
+Route::get('/lihat-guru', [GuruPublikController::class, 'index'])->name('lihat.guru');
+
+
+Route::get('/', function () {
+    $gurus = Guru::with('mapels')->get();
+    return view('publik.index', compact('gurus'));
+});
